74e1c7dd94347f4b0da9733e3c159b5e3a537463
bozagina <bbbforbazinga@gmail.com>
2026-02-28 22:10:38 +0800

    修复内容：
    在 modeling_mapanything_llava3d_vlm.py (line 9) 增加：
    import torch.nn.functional as F

--- starVLA/mapanything_llava3d/model/modeling_mapanything_llava3d_vlm.py
+++ starVLA/mapanything_llava3d/model/modeling_mapanything_llava3d_vlm.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from transformers import PreTrainedModel, AutoModel
 from transformers.utils import ModelOutput, logging
 from transformers.generation import GenerationMixin
